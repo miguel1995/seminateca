94d9b253ab2ab0f06b896ddb471360cc6aa0497d
miatorresch <miatorresch@unal.edu.co>
2022-02-19 10:21:27 -0500

    Fragments base y grafico de navegación

--- app/src/main/java/com/miatorresch/seminateca/MainActivity.kt
+++ app/src/main/java/com/miatorresch/seminateca/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.miatorresch.seminateca
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.options_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.
+        onNavDestinationSelected(item,Navigation.findNavController(this, R.id.myNavHostFragment))
+                || super.onOptionsItemSelected(item)
+    }
+
 }
